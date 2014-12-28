46d104ccda8bf74664699994926e73d0f6eede71
Danielle Vass <danielle@de-velopment.com>
2014-12-28 16:06:42 +0000

    adding in attributes

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ function createObjects() {
   emoji1 = {
     'cx': 100,
     'cy': 100,
-    'fill': '#ffcc00'
+    'fill': '#ffcc00',
+    'eyeColour': '#0D47A1'
   };
 
   emoji2 = {
     'cx': 300,
     'cy': 100,
-    'fill': '#C62828'
+    'fill': '#C62828',
+    'eyeColour': '#558B2F'
   };
 
   emoji3 = {
     'cx': 500,
     'cy': 100,
-    'fill': '#1565C0'
+    'fill': '#1565C0',
+    'eyeColour': '#B71C1C'
   };
 
   emojis.push(emoji1);
@@ -26,25 +29,30 @@ function createObjects() {
   emojis.push(emoji3);
 
 
-
 }
 
 
 function drawShapes() {
 
-  for(var i in emojis) {
+  for (var i in emojis) {
 
     var emoji = emojis[i];
 
-    var circle = paper.circle(emoji.cx, emoji.cy, 50);
-    circle.attr("fill", emoji.fill);
+    var face = paper.circle(emoji.cx, emoji.cy, 50);
+    face.attr("fill", emoji.fill);
+
+    var eyeLeft = paper.circle(emoji.cx - 18, emoji.cy - 5, 3);
+    eyeLeft.attr("fill", emoji.eyeColour);
+
+    var eyeRight = paper.circle(emoji.cx + 18, emoji.cy - 5, 3);
+    eyeRight.attr("fill", emoji.eyeColour);
+
 
 
   }
 
 
 }
-
 
 
 createObjects();
