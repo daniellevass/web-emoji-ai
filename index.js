a6bb1d0d2a6f984fba763b0fa5615f1de39fe0ac
Danielle Vass <danielle@de-velopment.com>
2014-12-28 15:51:54 +0000

    create functions

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var paper = Raphael(0, 0, 700, 700);
+var emoji1;
 
-var emoji1 = {'cx': 100,
-'cy': 100,
-'fill': '#ffcc00'};
+function createObjects(){
+
+  emoji1 = {'cx': 100,
+  'cy': 100,
+  'fill': '#ffcc00'};
 
 
-var circle = paper.circle(emoji1.cx, emoji1.cy, 50);
-circle.attr("fill", emoji1.fill);
+}
+
+
+function drawShapes(){
+
+  var circle = paper.circle(emoji1.cx, emoji1.cy, 50);
+  circle.attr("fill", emoji1.fill);
+
+}
+
+
+
+createObjects();
+drawShapes();
